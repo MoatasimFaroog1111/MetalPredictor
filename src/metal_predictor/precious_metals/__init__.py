0305c162f07575e12849263279a0f4655a94e631
MoatasimFaroog1111 <moatasim1111@gmail.com>
2026-08-12 15:31:22 +0300

    Export precious metals provenance gate

--- src/metal_predictor/precious_metals/__init__.py
+++ src/metal_predictor/precious_metals/__init__.py
@@ -6,10 +6,20 @@ from metal_predictor.precious_metals.contracts import (
 )
 from metal_predictor.precious_metals.dukascopy_source import DukascopyHistoricalMetalSource
 from metal_predictor.precious_metals.features import PlatinumPalladiumCrossAssetFeatures
+from metal_predictor.precious_metals.provenance import (
+    HistoricalBootstrapAssessment,
+    HistoricalBootstrapManifest,
+    HistoricalBootstrapPolicy,
+    HistoricalBootstrapProvenanceGate,
+)
 
 __all__ = [
     "HistoricalMetalSource",
     "PreciousMetalInstrument",
     "DukascopyHistoricalMetalSource",
     "PlatinumPalladiumCrossAssetFeatures",
+    "HistoricalBootstrapAssessment",
+    "HistoricalBootstrapManifest",
+    "HistoricalBootstrapPolicy",
+    "HistoricalBootstrapProvenanceGate",
 ]
