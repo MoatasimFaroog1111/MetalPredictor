831d71a8a78b9726be846d84cab6c0b0151ccf70
MoatasimFaroog1111 <moatasim1111@gmail.com>
2026-08-13 15:58:21 +0300

    Export Stage 5 admission and forecast components

--- src/metal_predictor/forward_bars/__init__.py
+++ src/metal_predictor/forward_bars/__init__.py
@@ -1,3 +1,9 @@
+from metal_predictor.forward_bars.admission import (
+    ADMISSION_POLICY_VERSION,
+    MINIMUM_COVERAGE_RATIO,
+    AdmissionDecision,
+    ForwardBarAdmissionPolicy,
+)
 from metal_predictor.forward_bars.alignment import EPOCH_UTC, bucket_start_for, latest_eligible_bucket_end
 from metal_predictor.forward_bars.contracts import (
     FORWARD_BAR_VERSION,
@@ -10,20 +16,32 @@ from metal_predictor.forward_bars.contracts import (
     QuoteSampleSource,
 )
 from metal_predictor.forward_bars.factory import ForwardBarFactory
+from metal_predictor.forward_bars.forecasting import (
+    BASELINE_ID,
+    FORECAST_POLICY_VERSION,
+    MultiHorizonBaselineForecastService,
+)
 from metal_predictor.forward_bars.repository import SQLiteForwardBarRepository
 from metal_predictor.forward_bars.scheduler import ForwardBarMaterializationScheduler
 from metal_predictor.forward_bars.source import SQLiteMicrostructureQuoteSampleSource
 
 __all__ = [
+    "ADMISSION_POLICY_VERSION",
+    "AdmissionDecision",
+    "BASELINE_ID",
     "BucketAssessment",
     "EPOCH_UTC",
+    "FORECAST_POLICY_VERSION",
     "FORWARD_BAR_VERSION",
     "FORWARD_HORIZON_SECONDS",
     "FORWARD_SOURCE_STREAM",
     "ForwardBar",
+    "ForwardBarAdmissionPolicy",
     "ForwardBarFactory",
     "ForwardBarMaterializationScheduler",
     "ForwardBarRepository",
+    "MINIMUM_COVERAGE_RATIO",
+    "MultiHorizonBaselineForecastService",
     "QuoteSample",
     "QuoteSampleSource",
     "SQLiteForwardBarRepository",
